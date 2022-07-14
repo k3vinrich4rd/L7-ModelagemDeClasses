4aafc26dcbcafab27019f8f7b95e10136c501cd1
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-13 23:28:20 -0300

    Inserindo segunda resolução de classe

--- src/pais/Pais.java
+++ src/pais/Pais.java
@@ -4,39 +4,87 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pais {
-    private String nome;
-    private int populacao;
+
+    private String codigoIso;
+    private String nomeDoPais;
+    private double populacao;
+    private double dimensao;
     private List<String> listaVizinhos = new ArrayList<>();
 
-    public Pais(String nome, int populacao, List<String> listaVizinhos) {
-        this.nome = nome;
+    public Pais(String codigoIso, String nomeDoPais, double populacao, double dimensao, List<String> listaVizinhos) {
+        this.codigoIso = codigoIso;
+        this.nomeDoPais = nomeDoPais;
         this.populacao = populacao;
+        this.dimensao = dimensao;
         this.listaVizinhos = listaVizinhos;
+    }
 
+    public void statusBrasil() {
+        System.out.println("--------------------------------");
+        System.out.println("Informações referente ao país: ");
+        System.out.println("--------------------------------");
+        System.out.println("Código ISO: " + this.codigoIso + "\nNome do pais: " + this.nomeDoPais +
+                "\nNúmero populacional: " + this.populacao + " milhões de habitantes " + "\nDimensão: "
+                + this.dimensao + " km²" + "\nPossui fronteiras com: " + this.listaVizinhos);
+        System.out.println("--------------------------------");
 
     }
 
-    public boolean verificacaoFronteira(String nomePais){
-        if(listaVizinhos.contains(nomePais)) {
+
+
+    public boolean verificacaoFronteira(String nomePais) {
+        if (listaVizinhos.contains(nomePais)) {
             return true;
-        }else {
+        } else {
             return false;
         }
-    }
-    public String getNome() {
-        return nome;
+
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public double densidadeDemografica() {
+        return this.populacao / this.dimensao;
     }
 
-    public int getPopulacao() {
+    public String getCodigoIso() {
+        return codigoIso;
+    }
+
+    public void setCodigoIso(String codigoIso) {
+        this.codigoIso = codigoIso;
+    }
+
+    public String getNomeDoPais() {
+        return nomeDoPais;
+    }
+
+    public void setNomeDoPais(String nomeDoPais) {
+        this.nomeDoPais = nomeDoPais;
+    }
+
+    public double getPopulacao() {
         return populacao;
     }
 
-    public void setPopulacao(int populacao) {
+    public void setPopulacao(double populacao) {
         this.populacao = populacao;
     }
+
+    public double getDimensao() {
+        return dimensao;
+    }
+
+    public void setDimensao(double dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    public List<String> getListaVizinhos() {
+        return listaVizinhos;
+    }
+
+    public void setListaVizinhos(List<String> listaVizinhos) {
+        this.listaVizinhos = listaVizinhos;
+    }
+
+
 }
 
--- src/pais/PaisMain.java
+++ src/pais/PaisMain.java
@@ -6,12 +6,20 @@ import java.util.List;
 public class PaisMain {
     public static void main(String[] args) {
         List<String> vizinhosBrasil = new ArrayList<>();
+        Pais brasil = new Pais("BRA", "Brasil", 212.6, 8.516, vizinhosBrasil);
         vizinhosBrasil.add("Argentina");
+        vizinhosBrasil.add("Uruguai");
+        vizinhosBrasil.add("Paraguai");
+        vizinhosBrasil.add("Bolívia");
         vizinhosBrasil.add("Peru");
+        vizinhosBrasil.add("Colômbia");
+        vizinhosBrasil.add("Venezuela");
+        vizinhosBrasil.add("Guiana");
+        vizinhosBrasil.add("Guiana Francesa");
+        vizinhosBrasil.add("Suriname");
+        brasil.statusBrasil();
+        System.out.printf("A sua densidade demográfica é de: %.2f km²", brasil.densidadeDemografica());
 
-        Pais pais = new Pais("Brasil", 300, vizinhosBrasil);
-        System.out.println(pais.getNome());
-        System.out.println("A argentina fraz froenteira: " + pais.verificacaoFronteira("Peru"));
 
     }
 }
